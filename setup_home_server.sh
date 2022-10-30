c4c5784bca4061195921711902f1652dc6df72ba
Miller Arlindo Hall <hall@hey.com>
2022-10-30 15:51:36 -0400

    Initial ngingx config, make server only dir

---
+++ setup_home_server.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function setup_static_ip {
+  CONFIG_FILE="./server/netplan-home_server_netplan_installer"
+  if test (hostname -I | grep 192.168.1.200) ; then
+    return
+  fi
+
+  if ! ls "$CONFIG_FILE" ; then
+    echo "Please run this script from the dotfiles root directory, I don't want to do"
+    echo "all the work that I'd need to do in order to make this script not care about"
+    echo "where it is run..."
+    exit 1
+  fi
+
+  echo "Warning: This will reset the network to use IP 192.168.1.200, you will need to reconnect..."
+  cp "$CONFIG_FILE" /etc/netplan/00-installer-config.yaml
+  sudo netplan apply
+}
+
+function install_nginx {
+  sudo apt update -y
+  sudo apt install -y nginx
+}
+
+function install_pihole {
+  curl -sSL https://install.pi-hole.net | bash
+}
+
+function setup {
+  setup_static_ip
+  install_pihole
+}
+
+setup
